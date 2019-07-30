26b6d31029c05f6e8d363e0f11f3f899feb1c26a
Nguyễn Huy <seminerd>
2019-07-30 16:18:21 +0700

    1

--- src/main/java/com/sapo/team03/MCRM/Security/WebSecurityConfig.java
+++ src/main/java/com/sapo/team03/MCRM/Security/WebSecurityConfig.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.sapo.team03.MCRM.Exception.PasswordNotMatch;
 import com.sapo.team03.MCRM.Filters.JWTAuthenticationFilter;
 import com.sapo.team03.MCRM.Filters.JWTLoginFilter;
 
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception,PasswordNotMatch {
 //		auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("ADMIN");
 		auth.jdbcAuthentication().dataSource(dataSource)
 				.usersByUsernameQuery("select email,password,true as enabled from nhanvien where email=?")
