cba47acde092a47a395b33c71ab999b70259cc0b
tbngoc0910 <yttranngoc.hv@gmail.com>
2019-08-27 09:25:44 +0700

    simple login

--- src/main/java/com/sapo/team03/MCRM/Controller/StaffController.java
+++ src/main/java/com/sapo/team03/MCRM/Controller/StaffController.java
@@ -18,9 +18,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sapo.team03.MCRM.DAO.StaffDAO;
+import com.sapo.team03.MCRM.Exception.PasswordNotMatch;
+import com.sapo.team03.MCRM.Exception.UsernameNotFound;
 import com.sapo.team03.MCRM.Management.Model.Staff;
 import com.sapo.team03.MCRM.Utils.Utilities;
-@CrossOrigin(origins="*")
+
+@CrossOrigin(origins = "*")
 @RestController(value = "staffs")
 public class StaffController {
 	@Autowired
@@ -29,44 +32,63 @@ public class StaffController {
 	StaffDAO staffDAO;
 
 	@GetMapping("staffs/list")
-	public List<Staff> getAllStaff(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size) {
-		if(page != null && size != null) return staffDAO.findAll(PageRequest.of(page, size)).getContent();
-		if(page != null && size == null) return staffDAO.findAll(PageRequest.of(page, 20)).getContent();
+	public List<Staff> getAllStaff(@RequestParam(value = "page", required = false) Integer page,
+			@RequestParam(value = "size", required = false) Integer size) {
+		if (page != null && size != null)
+			return staffDAO.findAll(PageRequest.of(page, size)).getContent();
+		if (page != null && size == null)
+			return staffDAO.findAll(PageRequest.of(page, 20)).getContent();
 		return staffDAO.findAll(Sort.by(Sort.Direction.DESC, "updateDate"));
 	}
+
 	@GetMapping("staffs")
 	public Staff getStaffByEmail(@RequestParam(value = "email", required = true) String email) {
 		return staffDAO.findByEmail(email);
 	}
+
 	@PostMapping("staffs/add")
 	public Staff addStaff(@RequestBody Staff staff) {
 		String encoded = BCrypt.hashpw(staff.getPassword(), BCrypt.gensalt());
 		staff.setPassword(encoded);
 		staff.setUpdateDate(new Date());
 		return staffDAO.save(staff);
-		
+
 	}
 
 	@GetMapping("staffs/{id}")
 	public Staff findStaffById(@PathVariable("id") Long id) {
 		return staffDAO.findById(id).orElse(null);
 	}
+
 	@PutMapping("staffs/{id}")
 	public Staff editStaff(@PathVariable Long id, @RequestBody Staff staff) {
 		Staff sta = staffDAO.findById(id).orElse(null);
-		if(sta == null) return null;
-		if(staff.getCustomers()!= null) sta.setCustomers(staff.getCustomers());
-		if(staff.getDob()!= null) sta.setDob(staff.getDob());
-		if(staff.getOrders()!= null) sta.setOrders(staff.getOrders());
-		if(staff.getEmail()!= null) sta.setEmail(staff.getEmail());
-		if(staff.getTransactions()!= null) sta.setTransactions(staff.getTransactions());
-		if(staff.getGender()!= null) sta.setGender(staff.getGender());
-		if(staff.getName()!= null) sta.setName(staff.getName());
-		if(staff.getDescription()!= null) sta.setDescription(staff.getDescription());
-		if(staff.getPassword()!= null) sta.setPassword(BCrypt.hashpw(staff.getPassword(), BCrypt.gensalt()));
-		if(staff.getPhone()!= null) sta.setPhone(staff.getPhone());
-		if(staff.getRole()!= null) sta.setRole(staff.getRole());
-		if(staff.getJobTitle()!= null) sta.setJobTitle(staff.getJobTitle());
+		if (sta == null)
+			return null;
+		if (staff.getCustomers() != null)
+			sta.setCustomers(staff.getCustomers());
+		if (staff.getDob() != null)
+			sta.setDob(staff.getDob());
+		if (staff.getOrders() != null)
+			sta.setOrders(staff.getOrders());
+		if (staff.getEmail() != null)
+			sta.setEmail(staff.getEmail());
+		if (staff.getTransactions() != null)
+			sta.setTransactions(staff.getTransactions());
+		if (staff.getGender() != null)
+			sta.setGender(staff.getGender());
+		if (staff.getName() != null)
+			sta.setName(staff.getName());
+		if (staff.getDescription() != null)
+			sta.setDescription(staff.getDescription());
+		if (staff.getPassword() != null)
+			sta.setPassword(BCrypt.hashpw(staff.getPassword(), BCrypt.gensalt()));
+		if (staff.getPhone() != null)
+			sta.setPhone(staff.getPhone());
+		if (staff.getRole() != null)
+			sta.setRole(staff.getRole());
+		if (staff.getJobTitle() != null)
+			sta.setJobTitle(staff.getJobTitle());
 		sta.setUpdateDate(new Date());
 		staffDAO.save(sta);
 		return staffDAO.findById(id).get();
@@ -76,5 +98,16 @@ public class StaffController {
 	public void deleteStaff(@PathVariable Long id) {
 		staffDAO.deleteById(id);
 	}
-	
+
+	@PostMapping("staff/login")
+	public Staff checkPassword(@RequestBody Staff staff) {
+		Staff temp = staffDAO.findByEmail(staff.getEmail());
+		if (temp == null)
+			throw new UsernameNotFound(staff.getEmail());
+		if (!BCrypt.checkpw(staff.getPassword(), temp.getPassword()))
+			throw new PasswordNotMatch();
+		else
+			return temp;
+	}
+
 }
